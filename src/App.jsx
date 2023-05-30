6cfb909ef27f1d08d5e3596ea0bc6cfa415baada
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-05-30 10:36:10 -0300

    Navbar responsiva

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from './components/HeaderComponent/Header';
 import FooterComponent from './components/FooterComponent/FooterComponent';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
     <div className="app">
-      <Header />
-      <div className="page-container">
-        <Outlet />
+        <Header />
+        <Container fluid>
+          <div className="page-container">
+            <Outlet />
+          </div>
+        </Container>
+        <FooterComponent />
       </div>
-      <FooterComponent />
-    </div>
   )
 }
 
--- src/components/HeaderComponent/Header.jsx
+++ src/components/HeaderComponent/Header.jsx
@@ -1,29 +1,45 @@
-import "./header.css";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header() { 
   return (
-    <div className="header-top">
-      <div className="header-logo">Sintax</div>
-
-      <div className="header-links">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Sobre nós</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contato</Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="buttonLogin">
-        <button type="button"><Link to="/login">Login/Cadastro</Link> </button>
-      </div>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <h1>Sintax</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link as={Link} to="/">
+              <h4>Home</h4>
+            </Nav.Link>
+            <Nav.Link></Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              <h4>Sobre</h4>
+            </Nav.Link>
+            <Nav.Link></Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              <h4>Contato</h4>
+            </Nav.Link>
+            <Nav.Link></Nav.Link>
+          </Nav>
+          <Button variant="outline-primary">
+            <Nav.Link as={Link} to="/cad-login">
+              Login / Cadastro
+            </Nav.Link>
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 export default Header;
